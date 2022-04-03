2628c26e2d2da56099ec4ec5069a93a0b66a713d
Leon To <khoito100@gmail.com>
2022-04-03 15:11:07 -0400

    'init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex h-screen">
+    <top-nav-bar class="flex-0 w-16 h-full"/>
+    <side-nav-bar class="flex-0 w-32" />
+    <content-list class="flex-0 w-64"/>
+  </div>
+</template>
+
+<script>
+import ContentList from '~/components/ContentList.vue'
+import TopNavBar from '~/components/TopNavBar.vue'
+import SideNavBar from '~/components/SideNavBar.vue'
+
+export default {
+components: { TopNavBar, SideNavBar, ContentList },
+  name: 'IndexPage'
+}
+
+</script>
